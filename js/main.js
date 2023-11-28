208bbb4b2c5cb5e39c7e123c1ae5db6a25abc71c
nemhee <bayrnemeh33@gmail.com>
2023-11-28 11:01:17 +0800

    index query

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const toggless = document.getElementById("toggle");
+
+hamburger.addEventListener("click", function(){
+toggless.classList.toggle("active1");    
+})
