93cb512043ee9d05fab9c598d8f2bbae65007bb2
nemhee <bayrnemeh33@gmail.com>
2023-11-29 08:54:41 +0800

    lab13

--- js/tools.js
+++ js/tools.js
@@ -1,37 +1,44 @@
-export default class Tools {
+export default class Tool {
     constructor(name, price, category) {
         this.name = name;
         this.price = price;
         this.category = category;
     }
-}
 
-export async function fetchAndRenderTools() {
-    try {
-        const response = await fetch('tools.json');
-        console.log('Response status:', response.status);
-        const tools = await response.json();
-        const categoryParam = new URLSearchParams(window.location.search).get('category');
-        const filteredTools = categoryParam ? tools.filter(tool => tool.category === categoryParam) : tools;
-        renderTools(filteredTools);
-    } catch (error) {
-        console.error('Error fetching or rendering tools:', error);
-    }
-}
+    async fetchAndRenderTools(target) {
+        try {
+            const response = await fetch('tools.json');
 
-function renderTools(tools) {
-    const toolsContainer = document.getElementById('tools-container');
-    if (!toolsContainer) {
-        console.error("error: tools container not found in the DOM");
-        return;
+            if (!response.ok) {
+                throw new Error(`Failed to fetch tools. Status: ${response.status}`);
+            }
+
+            const tools = await response.json();
+            const categoryParam = new URLSearchParams(window.location.search).get('category');
+            const filteredTools = categoryParam ? tools.filter(tool => tool.category === categoryParam) : tools;
+
+            this.renderTools(filteredTools, target);
+        } catch (error) {
+            console.error('Error fetching or rendering tools:', error);
+        }
     }
-    toolsContainer.innerHTML = '';
-    tools.forEach(tool => {  
-        const toolElement = document.createElement('div');
-        toolElement.innerHTML = `
-            <h3>${tool.name}</h3>
-            <p>Price: ${tool.price}₮</p>
-        `;
-        toolsContainer.appendChild(toolElement);
-    });
+
+    renderTools(tools, target) {
+        const toolsContainer = document.getElementById(target);
+
+        if (!toolsContainer) {
+            console.error("Error: tools container not found in the DOM");
+            return;
+        }
+
+        toolsContainer.innerHTML = '';
+        tools.forEach(tool => {
+            const toolElement = document.createElement('article');
+            toolElement.innerHTML = `
+                <h3>${tool.name}</h3>
+                <p>Price: ${tool.price}₮</p>
+            `;
+            toolsContainer.appendChild(toolElement);
+        });
+    }
 }
